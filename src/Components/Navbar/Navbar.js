c44087cdbc79c4ece7e30738b3c7038e507e6669
tej00001 <tejachintu207@gmail.com>
2023-03-02 15:27:00 +0530

    Adding Task 23 Protecting the product page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -34,6 +34,7 @@ const NavbarList = () => {
             >
               HOME
             </NavLink>
+            
             {authCtx.isLoggedIn && (
               <NavLink
                 to="/STORE"
